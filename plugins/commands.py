cf622ef0e909c0ae85ae31bf702882ae3974cf71
Gaxi2 <103119685+Gaxi2@users.noreply.github.com>
2022-04-16 12:53:47 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -39,7 +39,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('⚚ ΛᎠᎠ MΞ ϮԾ YԾUᏒ GᏒԾUᎮ ⚚', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('⚚ Join Our GᏒouᎮ ⚚', url='http://t.me/findhub2')
             ],[
             InlineKeyboardButton('💠 CHΛИИΞL 💠', url='https://t.me/findhubupdates'),
             InlineKeyboardButton('💠 UᎮDΛTΞS 💠', url='https://t.me/findhubupdates')
@@ -82,7 +82,7 @@ async def start(client, message):
         return
     if len(message.command) ==2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('⚚ Join Our GᏒouᎮ ⚚', url=f'http://t.me/findhub2')
+            InlineKeyboardButton('⚚ Join Our GᏒouᎮ ⚚', url='http://t.me/findhub2')
             ],[
             InlineKeyboardButton('💠 CHΛИИΞL 💠', url='https://t.me/findhubupdates'),
             InlineKeyboardButton('💠 UᎮDΛTΞS 💠', url='https://t.me/findhubupdates')
